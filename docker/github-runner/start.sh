aa93989029ba482a518960378771ff05321f7ac7
Karsten Jakobsen <karsten@karstenjakobsen.dk>
2025-10-04 12:09:22 +0000

    chore: test release 3

---
+++ docker/github-runner/start.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+REG_TOKEN=$(curl -X POST -H "Authorization: token ${ACCESS_TOKEN}" -H "Accept: application/vnd.github+json" https://api.github.com/repos/"${REPOSITORY}"/actions/runners/registration-token | jq .token --raw-output)
+
+cd /home/docker/actions-runner || return
+
+./config.sh --url https://github.com/"${REPOSITORY}" --token "${REG_TOKEN}" --unattended --replace --name "${RUNNER_NAME}" --labels "${RUNNER_LABELS}"
+
+cleanup() {
+    echo "Removing runner..."
+    ./config.sh remove --unattended --token "${REG_TOKEN}"
+}
+
+trap 'cleanup; exit 130' INT
+trap 'cleanup; exit 143' TERM
+
+./run.sh & wait $!
