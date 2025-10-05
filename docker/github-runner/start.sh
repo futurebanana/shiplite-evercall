65b99b15f497dc7cdf4c97f68bc2d40c7c69862a
Karsten Jakobsen <karsten@karstenjakobsen.dk>
2025-10-05 15:40:08 +0000

    fix: test

--- docker/github-runner/start.sh
+++ docker/github-runner/start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "Configuring runner with name ${RUNNER_NAME} for repository ${REPOSITORY} and tokens ${REG_TOKEN}..."
+
 REG_TOKEN=$(curl -X POST -H "Authorization: token ${ACCESS_TOKEN}" -H "Accept: application/vnd.github+json" https://api.github.com/repos/"${REPOSITORY}"/actions/runners/registration-token | jq .token --raw-output)
 
 cd /home/docker/actions-runner || return
